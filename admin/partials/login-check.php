c6cd24b0447d70b0c747ca1627ab8450f9d074c0
SanduNissanka <153540879+SanduNissanka@users.noreply.github.com>
2024-05-31 21:39:09 +0530

    Add files via upload

---
+++ admin/partials/login-check.php
@@ -0,0 +1,16 @@
+<?php
+    // Authorization and access control
+    // check whether the user is logged in or not
+    if (!isset($_SESSION['user']))  // if user session is not set
+    {
+        // user is not logged in
+        //redirect to login page with massage..
+
+        $_SESSION['no-login-message']="<div class='error' >Please Login to access admin panel.</div>";
+
+        // redirect to login page
+
+        header('location:'.SITEURL.'admin/login.php');
+    }
+
+?>
